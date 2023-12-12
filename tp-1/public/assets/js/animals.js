8b5c59948e556f1e00bdde363a6b747a060e7508
Belen M <maria.moine@est.fi.uncoma.edu.ar>
2023-12-12 08:49:33 -0300

    update all .js files front
    mejora para utilizacion de promesas en todos los archivos .js del front

--- tp-1/public/assets/js/animals.js
+++ tp-1/public/assets/js/animals.js
@@ -1,17 +1,23 @@
 let currentId = 1;
 
-const createAnimalCard = () => fetch("/api/animal/" + currentId)
-    .then((response) => response.json())
-    .then((animal) => {
+const createAnimalCard = async () => {
+    try {
+        const response = await fetch("/api/animal/" + currentId);
+        const animal = await response.json();
+
         // Elements
         let animalCard = document.createElement("a");
         let name = document.createElement("h3");
+
         // Animal
         animalCard.classList.add("animal-card");
+
         // Image
         animalCard.style.backgroundImage = "url('assets/media/backgrounds/" + animal.id + ".jpg')";
+
         // Link
         animalCard.setAttribute("href", "assets/html/animal.html#" + animal.id);
+
         // Name
         name.classList.add("text");
         name.classList.add("card-name");
@@ -19,21 +25,29 @@ const createAnimalCard = () => fetch("/api/animal/" + currentId)
 
         animalCard.appendChild(name);
         document.querySelector(".animal-cards").appendChild(animalCard);
-    });
+    } catch (error) {
+        console.error('Error al crear la tarjeta del animal:', error);
+    }
+};
 
-const loadMoreAnimals = () => fetch("/api/animals/amount")
-    .then((response) => response.json())
-    .then((amount) => {
+const loadMoreAnimals = async () => {
+    try {
+        const response = await fetch("/api/cantidadAnimales");
+        const amount = await response.json();
+
         // Add 3 more animals
         lastAnimalId = currentId + 2;
         while (currentId <= lastAnimalId) {
-            createAnimalCard();
+            await createAnimalCard();
             currentId++;
-            if (currentId > amount.amount) {
+            if (currentId > amount.cantidad) {
                 document.querySelector(".show-more-btn").classList.add("hidden");
                 currentId = lastAnimalId + 1;
             }
         }
-    });
+    } catch (error) {
+        console.error('Error al cargar más animales:', error);
+    }
+};
 
 loadMoreAnimals();
--- tp-1/public/assets/js/newAnimal.js
+++ tp-1/public/assets/js/newAnimal.js
@@ -1,36 +1,32 @@
-addAnimal = () => {
-    let newAnimal;
+addAnimal = async () => {
+    try {
+        const newAnimal = {
+            name: document.getElementById('name').value,
+            scientificName: document.getElementById('scientificName').value,
+            description: document.getElementById('description').value,
+            weightHeight: document.getElementById('weightHeight').value,
+            zones: document.getElementById('zones').value,
+            diet: document.getElementById('diet').value,
+            extinction: document.getElementById('extinction').checked,
+        };
 
-    newAnimal = {
-        name: document.getElementById('name').value,
-        scientificName: document.getElementById('scientificName').value,
-        description: document.getElementById('description').value,
-        weightHeight: document.getElementById('weightHeight').value,
-        zones: document.getElementById('zones').value,
-        diet: document.getElementById('diet').value,
-        extinction: document.getElementById('extinction').checked,
-    };
-
-    fetch('/api/animales', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newAnimal)
-    })
-        .then(function (res) {
-            if (res.ok) {
-                document.querySelector(".success-text").classList.remove("hidden");
-                document.querySelector(".fail-text").classList.add("hidden");
-                return res;
-            }
-            else {
-                document.querySelector(".success-text").classList.add("hidden");
-                document.querySelector(".fail-text").classList.remove("hidden");
-                throw new Error('POST fallido.');
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
+        const response = await fetch('/api/agregarAnimal', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newAnimal)
         });
+
+        if (response.ok) {
+            document.querySelector(".success-text").classList.remove("hidden");
+            document.querySelector(".fail-text").classList.add("hidden");
+        } else {
+            document.querySelector(".success-text").classList.add("hidden");
+            document.querySelector(".fail-text").classList.remove("hidden");
+            throw new Error('POST fallido.');
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
