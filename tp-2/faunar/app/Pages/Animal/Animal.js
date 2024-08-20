418594ca7fdbf38ed33be3f910745c3d9ea5341c
Nicolás Comeron <nicolas.comeron@est.fi.uncoma.edu.ar>
2024-08-19 22:15:15 -0300

    Update details screen

--- tp-2/faunar/app/Pages/Animal/Animal.js
+++ tp-2/faunar/app/Pages/Animal/Animal.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground, Image } from 'react-native';
+import Onboarding from '../../Components/Onboarding/Onboarding';
 import globalStyles from '../../Components/GlobalStyles/GlobalStyles';
+import styles from './Styles';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default Animal = ({ route }) => {
-
-  const { item } = route.params;
+  const { item, img } = route.params;
+  const { width, height } = Image.resolveAssetSource(img);
 
   return (
-    <View style={globalStyles.container}>
-      <Text>{item.name}</Text>
+    <View style={[globalStyles.container]}>
+      <View style={styles.backgroundImageContainer}>
+        <ImageBackground
+          source={img}
+          style={[styles.backgroundImage, { aspectRatio: width / height }]}
+        />
+      </View>
+
+      <View style={styles.animalContainer}>
+        <LinearGradient
+          colors={['rgba(0,0,0,0.8)', 'gray']}
+          style={styles.backgroundGradient}
+        >
+          <View style={styles.animalInfoContainer}>
+            <View style={styles.animalHeader}>
+              <Text style={styles.animalNameText}>{item.name}</Text>
+              <Text style={styles.animalScientificNameText}>
+                {item.scientificName}
+              </Text>
+              {item.extinction && (
+                <Text style={styles.inExtinctionText}>
+                  Especie en peligro de extinción
+                </Text>
+              )}
+            </View>
+
+            <Onboarding animalCharacteristics={item} />
+          </View>
+        </LinearGradient>
+      </View>
     </View>
   );
-}
+};
---
+++ tp-2/faunar/app/Pages/Animal/Styles.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  backgroundImageContainer: {
+    flex: 0.5,
+  },
+  animalContainer: {
+    flex: 0.5,
+  },
+  animalInfoContainer: {
+    flex: 1,
+    marginTop: 15,
+  },
+  backgroundImage: {
+    width: '100%',
+    alignSelf: 'center',
+  },
+  backgroundGradient: {
+    flex: 1,
+    backgroundColor: 'orange',
+  },
+  animalHeader: {
+    paddingHorizontal: 15,
+    marginBottom: 15,
+  },
+  animalNameText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 5,
+  },
+  animalScientificNameText: {
+    fontSize: 17,
+    fontWeight: '400',
+    color: 'white',
+    marginBottom: 5,
+  },
+  inExtinctionText: {
+    fontSize: 17,
+    fontWeight: '300',
+    color: 'lightgray',
+    marginBottom: 5,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: '300',
+    color: 'white',
+    lineHeight: 20,
+    marginBottom: 5,
+  },
+});
+
+export default styles;
